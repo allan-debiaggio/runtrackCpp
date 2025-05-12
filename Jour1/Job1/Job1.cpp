90fbf71d46de0e37780d940234e833dbc2a25fcb
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-12 13:53:39 +0200

    Jour 1 Job 1

---
+++ Jour1/Job1/Job1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+    // Statement 1 : 
+
+    char c = '\x01';
+    short int p = 10;
+
+
+    // Statement 2 : 
+
+    x = p + 3; (= 13)
+    y = c + 1; (= 2)
+    z = p + c; (= 11)
+    w = 3 * p + 5 * c; (= 35)
+}
