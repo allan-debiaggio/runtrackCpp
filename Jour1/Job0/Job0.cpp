c7fb8fe306619763f80f2ccd1dca613ae46efaa0
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-12 13:35:00 +0200

    Jour 1 Job 0

---
+++ Jour1/Job0/Job0.cpp
@@ -0,0 +1,15 @@
+// Which parentheses should you remove ?
+
+a = (x+5)
+a = (x=y) + 2
+a = (x==y)
+(a<b) && (c<d)
+(i++) * (n+p)
+
+// My solution :
+
+a = x + 5
+a = (x=y) + 2
+a = x == y
+a < b && c < d
+i++ * (n+p)
