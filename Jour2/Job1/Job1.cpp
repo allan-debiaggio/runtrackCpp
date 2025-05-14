22b82de2938a6d7956c4f57d575a884838c043b7
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-14 10:32:30 +0200

    Jour 2 Job 1

--- Jour2/Job1/Job1.cpp
+++ Jour2/Job1/Job1.cpp
@@ -1,71 +1,20 @@
+// Rendre le programme lisible
+// do {} while (cout << "donnez un nombre > 0 ", cin >> n, n <= 0);
+
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-class Aquatique
+int main(void)
 {
-protected:
-    double speed;
-    string unit;
+    int n = 0;
 
-public:
-    Aquatique(double speed = 0.0, string unit = "m/s") : speed(speed), unit(unit) {}
-
-    virtual void nage()
+    do
     {
-        cout << "Je nage à une vitesse de " << speed << " " << unit << "." << endl;
+        cout << "donnez un nombre > 0 : ";
+        cin >> n;
     }
-};
-
-class Terrestre
-{
-protected:
-    double speed;
-    string unit;
-
-public:
-    Terrestre(double speed = 0.0, string unit = "m/s") : speed(speed), unit(unit) {}
-
-    virtual void marche()
-    {
-        cout << "Je marche à une vitesse de " << speed << " " << unit << "." << endl;
-    }
-};
-
-class Pingouin : public Aquatique, public Terrestre
-{
-private:
-    string nom;
-
-public:
-    Pingouin(string nom, double swimSpeed, double walkSpeed)
-        : Aquatique(swimSpeed, "m/s"), Terrestre(walkSpeed, "m/s"), nom(nom) {}
-
-    void sePresenter()
-    {
-        cout << "Bonjour, je suis un pingouin et je m'appelle " << nom << "." << endl;
-    }
-
-    void nage() override
-    {
-        cout << nom << " nage à une vitesse de " << Aquatique::speed << " " << Aquatique::unit << "." << endl;
-    }
-
-    void marche() override
-    {
-        cout << nom << " marche à une vitesse de " << Terrestre::speed << " " << Terrestre::unit << "." << endl;
-    }
-};
-
-int main()
-{
-    Pingouin p("Pingu", 2.5, 1.2);
-
-    p.sePresenter();
-    p.nage();
-    p.marche();
+    while(n <= 0);
 
     return 0;
 }
-
