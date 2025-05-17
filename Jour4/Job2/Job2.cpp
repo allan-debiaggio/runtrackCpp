f2757a9eb223bd5594d50ecececb3cfe7a72a51d
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-17 10:39:39 +0200

    Jour 4 Job 2

--- Jour4/Job2/Job2.cpp
+++ Jour4/Job2/Job2.cpp
@@ -6,20 +6,21 @@ int main(void)
 {
     int num1 = 42;
     int num2 = 12;
+    int num3 = 0;
 
     cout << "Number 1 : " <<  num1 << endl;
     cout << "Number 2 : " << num2 << endl;
 
     int* ptr_num1 = &num1;
     int* ptr_num2 = &num2;
-    int* ptr_supp = &num1;
+    int* ptr_num3 = &num3;
 
+    num3 = *ptr_num1;
     num1 = *ptr_num2;
-    num2 = *ptr_supp;
+    num2 = *ptr_num3;
 
 
-
-    cout << "Pointer 1 variable content : " << *ptr_num1 << endl;
-    cout << "Pointer 2 variable content : " << *ptr_num2 << endl;
+    cout << "Number 1 : " << num1 << endl;
+    cout << "Number 2 : " << num2 << endl;
 
 }
